faaafb65239bf6c29929bb7d4ba9d9c66ad89c36
Chetna <cchetnapatil@gmail.com>
2020-08-11 19:35:49 +0530

    initial

---
+++ src/app/Components/registration/registration.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import{HttpRequestService} from '../../services/user.service'
+  import { from } from 'rxjs';
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+registerForm:FormGroup
+  constructor(private fb:FormBuilder,private service:HttpRequestService) { }
+
+  ngOnInit(): void {
+    this.registerForm=this.fb.group({
+      name:['',Validators.required],
+      email:['',Validators.required],
+      password:['',Validators.required]
+
+    })
+  }
+get name(){
+  return this.registerForm.get('name');
+}
+get eamil(){
+  return this.registerForm.get('email');
+}
+get password(){
+  return this.registerForm.get('password');
+}
+register(){
+this.service.add(this.registerForm.value).subscribe(res =>{
+  console.log(res);
+})}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule}from '@angular/forms'
+import {CommonModule}from '@angular/common'
+import {HttpClient,HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from '../app/Components/registration/registration.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
